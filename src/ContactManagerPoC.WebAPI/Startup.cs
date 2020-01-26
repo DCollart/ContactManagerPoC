c6636f290abcd61bdff634427e4939f28e84f519
Dominic Collart <dominic.collart@gmail.com>
2020-01-26 18:38:13 +0100

    feat(init): add healthcheck

--- src/ContactManagerPoC.WebAPI/Startup.cs
+++ src/ContactManagerPoC.WebAPI/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -26,6 +27,7 @@ namespace ContactManagerPoC.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +43,8 @@ namespace ContactManagerPoC.WebAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseHealthChecks("/healthcheck");
 
             app.UseEndpoints(endpoints =>
             {
